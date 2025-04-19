5089d91a7896141d277db7b99d2c0c231ed91110
franco2730 <franco.rosales2130@gmail.com>
2025-04-19 12:46:02 -0300

    prueba-cambio-correo

--- 05 Funciones/TP-Funciones.py
+++ 05 Funciones/TP-Funciones.py
@@ -4,7 +4,7 @@
 def imprimir_hola_mundo():
     print("Hola Mundo!")
 
-imprimir_hola_mundo()
+imprimir_hola_mundo() 
 
 
 #* 2. Crear una función llamada saludar_usuario(nombre) que reciba como parámetro un nombre y devuelva un saludo personalizado. Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. Llamar a esta función desde el programa principal solicitando el nombre al usuario.
